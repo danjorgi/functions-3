6f81449a7a19d3743826ffd0eff8e1e9dbda92b2
Daniel Jorgensen <dan.r.jorgensen@gmail.com>
2023-09-26 03:46:09 -0600

    MethodsOne Problem 4 complete

--- arrayMethodsOne.js
+++ arrayMethodsOne.js
@@ -84,7 +84,8 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 // CODE HERE
-const myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+const myStrongest = monstersInYourPocket.filter((monster) => monster.CP > 200)
+console.log(myStrongest)
 
 
 
